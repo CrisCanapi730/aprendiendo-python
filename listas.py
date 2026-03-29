607c21f45428a6756a71dc9f77a7c865b3ad2ff5
CrisCanapi730 <cristiancanapi730@gmail.com>
2026-03-29 10:06:22 -0400

    metodos de agregar y eliminar listas

--- listas.py
+++ listas.py
@@ -1,5 +1,6 @@
 # Listas
 print("\nlistas: \n")
+
 lista = [1,2,3,4]
 lista_2 = [98,99,19]
 
@@ -9,15 +10,20 @@ print(lista_2)
 print("\nAgregar elementos a una lista\n")
 # append
 lista.append(5)
+lista.append(5)
+
 print(lista)
 
-print(len(lista))
-
+# metodo extend
 lista.extend(lista_2)
-
 print(lista)
 
+# metodo insert
+lista.insert(0, 77) # index, numero insertado
+print(lista)
 
-
-
-
+# metodo remove
+lista.remove(5) # Elimina el primer 5 encontrado no todos
+print(lista)
+lista.remove(5) # Elimina el segundo 5 encontrado no todos
+print(lista)
