6dc60be077e6caf234ca7212cbff55e54860fd2d
CrisCanapi730 <cristiancanapi730@gmail.com>
2026-03-29 10:19:40 -0400

    feat: mejorando codigo y agregando nuevas funciones

--- listas.py
+++ listas.py
@@ -11,8 +11,13 @@ print("\nAgregar elementos a una lista\n")
 # append
 lista.append(5)
 lista.append(5)
-
 print(lista)
+
+# metodo count: cuenta cuantas veces aparece un valor
+print()
+
+# metodo index
+print(lista.index(5))
 
 # metodo extend
 lista.extend(lista_2)
@@ -22,8 +27,25 @@ print(lista)
 lista.insert(0, 77) # index, numero insertado
 print(lista)
 
+print(f"\nMetodos de eliminacion: \n")
 # metodo remove
+print(f"\nMetodo remove: \n")
+
+print(lista)
+print(f"\nEliminando el primer 5: \n")
 lista.remove(5) # Elimina el primer 5 encontrado no todos
 print(lista)
+print(f"\nEliminando el segundo 5: \n")
 lista.remove(5) # Elimina el segundo 5 encontrado no todos
+print(lista)
+
+# metodo pop
+lista.pop()
+print(lista)
+
+lista.pop(4)
+print(lista)
+
+# metodo clear
+lista.clear()
 print(lista)
