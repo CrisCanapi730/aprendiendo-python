8d01022ea58218053e7900a7bc02d181c4c51878
CrisCanapi730 <cristiancanapi730@gmail.com>
2026-03-29 10:00:55 -0400

    first commit

---
+++ listas.py
@@ -0,0 +1,23 @@
+# Listas
+print("\nlistas: \n")
+lista = [1,2,3,4]
+lista_2 = [98,99,19]
+
+print(lista)
+print(lista_2)
+
+print("\nAgregar elementos a una lista\n")
+# append
+lista.append(5)
+print(lista)
+
+print(len(lista))
+
+lista.extend(lista_2)
+
+print(lista)
+
+
+
+
+
